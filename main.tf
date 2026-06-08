8fa273b06ce8a4e12260591e93b50115edf541c5
Chandra_Rana <ranasijan1112@gmail.com>
2026-06-08 11:48:28 +0000

    create VPC using terraform

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 #create a VPC
-resource "aws_vpc" "main" {
+resource "aws_vpc" "my-terraform-lab-VPC" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "main"
+    Name = "terraform-lab"
   }
 }
